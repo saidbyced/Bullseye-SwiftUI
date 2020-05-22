afdc855c4d36e63c822e8bec19f31da721360f75
Chris Eadie <chris.eadie@outlook.com>
2020-05-22 18:01:56 +0100

    Add first button-generated popup event

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
             Text("Welcome to my first app!")
                 .fontWeight(.semibold)
                 .foregroundColor(Color.green)
-            Button(action: {}) {
+            Button(action: {
+                print("Button pressed")
+                self.alertIsVisible = true
+            }) {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello there"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+                
             }
         }
     }

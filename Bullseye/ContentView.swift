9c2d8e8a7a8225584b0f6645e223ca02d1ff1331
Chris Eadie <chris.eadie@outlook.com>
2020-05-25 12:10:41 +0100

    Add slider State value output to hit me pop-up

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,21 +11,61 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-            Button(action: {
-                print("Button pressed")
-                self.alertIsVisible = true
-            }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-            }
-            .alert(isPresented: $alertIsVisible) { () -> Alert in
-                return Alert(title: Text("Hello there"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+            VStack {
+                Spacer()
+                // Target Row
+                HStack {
+                    Text("Put the bullseye as close as you can to:")
+                    Text("100")
+                }
+                Spacer()
                 
+                // Slider row
+                HStack {
+                    Text("1")
+                    Slider(value: self.$sliderValue, in: 1...100)
+                    Text("100")
+                }
+                Spacer()
+                
+                // Button row
+                Button(action: {
+                    print("Button pressed")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    return Alert(title: Text("Hello there"), message: Text("The slider's value is \(Int(self.sliderValue))."), dismissButton: .default(Text("Awesome!")))
+                }
+                Spacer()
+                
+                // Score row
+                HStack {
+                    Button(action: {
+                        print("Resetting game")
+                    }) {
+                        Text("Start Over")
+                    }
+                    Spacer()
+                    Text("Score:")
+                    Text("999999")
+                    Spacer()
+                    Text("Round:")
+                    Text("999")
+                    Spacer()
+                    Button(action: {
+                        print("Pop-up some info")
+                    }) {
+                        Text("Info")
+                    }
+                    
+                }
+                .padding(.bottom, 20)
             }
         }
     }
@@ -33,6 +73,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
